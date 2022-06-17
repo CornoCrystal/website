270d76e3c2c5dbac4e2e4e9fcf2bc42919c5b260
yuri <maizenadiscord@gmail.com>
2022-06-17 16:26:43 -0300

    me diz que agora vai

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,33 @@
-<title>Corno Crystal</title>
 
-<h1>Somos um servidor de Minecraft de treino de Crystal PvP com anti-cheat não strict. </h1>
+<div class="main">
+    <title>Corno Crystal</title>
 
-<!-- <p>Caros amigos, a hegemonia do ambiente político nos obriga à análise das diretrizes de desenvolvimento para o futuro. Gostaria de enfatizar que a percepção das dificuldades pode nos levar a considerar a reestruturação das condições financeiras e administrativas exigidas. Assim mesmo, a contínua expansão de nossa atividade deve passar por modificações independentemente das diversas correntes de pensamento.<br><br>Evidentemente, o novo modelo estrutural aqui preconizado garante a contribuição de um grupo importante na determinação do investimento em reciclagem técnica. O cuidado em identificar pontos críticos no desenvolvimento contínuo de distintas formas de atuação nos obriga à análise das direções preferenciais no sentido do progresso. Percebemos, cada vez mais, que a constante divulgação das informações estimula a padronização do sistema de formação de quadros que corresponde às necessidades.<br><br>É importante questionar o quanto a competitividade nas transações comerciais não pode mais se dissociar das regras de conduta normativas. Acima de tudo, é fundamental ressaltar que o desafiador cenário globalizado estende o alcance e a importância do orçamento setorial. Neste sentido, o início da atividade geral de formação de atitudes acarreta um processo de reformulação e modernização de todos os recursos funcionais envolvidos. É claro que a valorização de fatores subjetivos é uma das consequências do levantamento das variáveis envolvidas.</p> -->
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <iframe src="https://discord.com/widget?id=785943859845660732&theme=dark" width="350" height="500" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"/>
 
-<h2>IP: CornoCrystal.com</h2>
+    <h1>Somos um servidor de Minecraft de treino de Crystal PvP com anti-cheat não strict. </h1>
+
+    <!-- <p>Caros amigos, a hegemonia do ambiente político nos obriga à análise das diretrizes de desenvolvimento para o futuro. Gostaria de enfatizar que a percepção das dificuldades pode nos levar a considerar a reestruturação das condições financeiras e administrativas exigidas. Assim mesmo, a contínua expansão de nossa atividade deve passar por modificações independentemente das diversas correntes de pensamento.<br><br>Evidentemente, o novo modelo estrutural aqui preconizado garante a contribuição de um grupo importante na determinação do investimento em reciclagem técnica. O cuidado em identificar pontos críticos no desenvolvimento contínuo de distintas formas de atuação nos obriga à análise das direções preferenciais no sentido do progresso. Percebemos, cada vez mais, que a constante divulgação das informações estimula a padronização do sistema de formação de quadros que corresponde às necessidades.<br><br>É importante questionar o quanto a competitividade nas transações comerciais não pode mais se dissociar das regras de conduta normativas. Acima de tudo, é fundamental ressaltar que o desafiador cenário globalizado estende o alcance e a importância do orçamento setorial. Neste sentido, o início da atividade geral de formação de atitudes acarreta um processo de reformulação e modernização de todos os recursos funcionais envolvidos. É claro que a valorização de fatores subjetivos é uma das consequências do levantamento das variáveis envolvidas.</p> -->
+
+    <h2>IP: CornoCrystal.com</h2>
+</div>
 
 <style lang="scss">
     @import '../styles/vars.scss';
+
+    iframe {
+        float: right;
+    }
+
+    .main {
+        margin: 50px;
+    }
 
     h1, h2 {
         text-align: left;
         position: relative;
         padding-right: 50%;
         left: 0;
-        margin: 50px;
         color: white;
     }
 
@@ -23,4 +36,6 @@
         color: $color-3;
         text-shadow: 0 0 5px $color-3
     }
+
+
 </style>
---
+++ src/routes/media.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Youtube from "$lib/components/Youtube.svelte";
+
+    let videos = [
+        "DCdKl_VziGQ",
+        "w_ulE7kSDzE",
+        "azSt2a761gw",
+        "zw9ISMN2G10",
+        "GR893Wftu5M",
+        "4hybIemF33U",
+        "Ayg4PXwV5sY"
+    ]
+</script>
+
+<div class="videos">
+    {#each videos as video}
+        <Youtube videoID={video}/>
+    {/each}
+</div>
+
+<style lang="scss">
+    .videos {
+        display: inline-block;
+    }
+</style>
